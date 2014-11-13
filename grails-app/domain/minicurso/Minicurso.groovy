6320286fb15c5568fe1e84b33cc027c957e98364
Ederson Garcia Elias <edersongarcia@outlook.com>
2014-11-13 02:18:23 -0200

    alterando dataFIm miniCurso  nullabel ( pode ser nulo)

--- grails-app/domain/minicurso/Minicurso.groovy
+++ grails-app/domain/minicurso/Minicurso.groovy
@@ -4,12 +4,14 @@ class Minicurso {
     String nome
     BigDecimal cargaHoraria
     Date dataIncio
+    Date dataFim
     Date dateCreated
     Date lastUpdated
     int vagas
     static hasMany = [inscricoes: Inscricao]
 
     static constraints = {
+        dataFim nullable: true
     }
 
     String toString(){
