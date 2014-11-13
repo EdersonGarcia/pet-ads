3ed1716765338ae84cbeb8bde29b8faf688d4f16
Ederson Garcia Elias <edersongarcia@outlook.com>
2014-11-13 03:24:33 -0200

    script para crianção do primeiro usuario do sistema

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,27 @@
+import seguranca.Permisao
+import seguranca.Usuario
+import seguranca.UsuarioPermisao
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Usuario.count < 1 && Permisao.count <1 && UsuarioPermisao.count()<1) {
+            println "criando Usuario Padrão"
+            def adminRole = new Permisao(authority: 'ROLE_ADMIN').save(flush: true)
+            def userRole = new Permisao(authority: 'ROLE_USUARIO').save(flush: true)
+
+            def testUser = new Usuario(username: 'admin', password: 'admin')
+            testUser.save(flush: true)
+
+            UsuarioPermisao.create testUser, adminRole, true
+
+            assert Usuario.count() == 1
+            assert Permisao.count() == 2
+            assert UsuarioPermisao.count() == 1
+            println "criado: $adminRole $userRole $testUser"
+        }else{
+            println("Usuario Padrao ja cadastrado")
+        }
     }
     def destroy = {
     }
